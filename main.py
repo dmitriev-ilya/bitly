9b57f83bc7f366a4bff654605b4b46672504da36
Ilusha <67222917+dmitriev-ilya@users.noreply.github.com>
2022-10-22 13:05:10 +0500

    Исправлено форматирование кода

--- main.py
+++ main.py
@@ -5,31 +5,41 @@ import argparse
 
 
 def shorten_link(token, url):
-    bitly_response = requests.post('https://api-ssl.bitly.com/v4/bitlinks',
-                                   json={"long_url": url},
-                                   headers={"Authorization" : f"Bearer {token}"})
+    bitly_response = requests.post(
+        'https://api-ssl.bitly.com/v4/bitlinks',
+        json={"long_url": url},
+        headers={"Authorization": f"Bearer {token}"}
+    )
     bitly_response.raise_for_status()
     bitlink = bitly_response.json()['id']
     return bitlink
 
 
 def count_clicks(token, bitlink):
-    bitly_response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary', 
-                                  headers={"Authorization": f"Bearer {token}"})
+    bitly_response = requests.get(
+        f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary', 
+        headers={"Authorization": f"Bearer {token}"}
+    )
     bitly_response.raise_for_status()
     clicks_count = bitly_response.json()['total_clicks']
     return clicks_count
 
 
 def is_bitlink(token, url):
-    bitly_response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{url}', 
-                                  headers={"Authorization": f"Bearer {token}"})
+    bitly_response = requests.get(
+        f'https://api-ssl.bitly.com/v4/bitlinks/{url}', 
+        headers={"Authorization": f"Bearer {token}"}
+    )
     return bitly_response.ok
+
 
 if __name__ == '__main__':
     load_dotenv()
     token = os.environ['BITLY_TOKEN']
-    parser = argparse.ArgumentParser(description='Программа делает Bitly ссылки и показывает количество их использования')
+    parser = argparse.ArgumentParser(
+        description='Программа делает Bitly ссылки \
+        и показывает количество их использования'
+    )
     parser.add_argument('users_url', help='Ваша ссылка')
     args = parser.parse_args()
     users_url = args.users_url
